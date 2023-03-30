816d546a705d63e9e8caab9fb73ed8ff0726d4d9
Filip Napiorkowski <filip.napiorkowski@xtremepush.com>
2023-03-30 15:31:33 +0100

    adding the android ejected version of the app

---
+++ my-app/xtremepush.js
@@ -0,0 +1,32 @@
+
+import { NativeModules } from 'react-native';
+
+const Xtremepush = require('react-native').NativeModules.Xtremepush;
+
+export default Xtremepush;
+
+function hitEvent(event){
+    Xtremepush.hitEvent(event);
+}
+
+function hitTag(tag){
+    Xtremepush.hitTag(tag);
+}
+
+function hitTagWithValue(tag, value){
+    Xtremepush.hitTagWithValue(tag, value);
+}
+
+function openInbox(){
+    Xtremepush.openInbox();
+}
+
+function setUser(user){
+    Xtremepush.setUser(user);
+}
+
+function setExternalId(id){
+    Xtremepush.setExternalId(id);
+}
+
+
