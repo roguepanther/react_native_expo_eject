1b0bf3f791a0c48b1051d102a6a8ac4f8208cc01
Filip Napiorkowski <filip.napiorkowski@xtremepush.com>
2023-03-30 10:45:11 +0100

    expo ejected and native modules added - XP integration in

---
+++ my-app/ios/RNXtremepushReact.m
@@ -0,0 +1,44 @@
+#import "RNXtremepushReact.h"
+#import <XPush/XPush.h>
+
+@implementation RNXtremepushReact
+
+- (dispatch_queue_t)methodQueue
+{
+    return dispatch_get_main_queue();
+}
+
+
+RCT_EXPORT_METHOD(hitEvent : (NSString *)event)
+{
+    [XPush hitEvent : event];
+}
+
+RCT_EXPORT_METHOD(hitTag : (NSString *)tag)
+{
+    [XPush hitTag : tag];
+}
+
+RCT_EXPORT_METHOD(hitTagWithValue : (NSString *)tag withValue: (NSString *)value)
+{
+    [XPush hitTag : tag withValue:value];
+}
+
+RCT_EXPORT_METHOD(setUser : (NSString *)user)
+{
+    [XPush setUser : user];
+}
+
+RCT_EXPORT_METHOD(setExternalId : (NSString *)id)
+{
+    [XPush setExternalId : id];
+}
+
+RCT_EXPORT_METHOD(openInbox)
+{
+    [XPush openInbox];
+}
+
+RCT_EXPORT_MODULE(Xtremepush)
+@end
+  
